f57fea99c1be0818141676d3e98391ac0e91b576
Dana Elisa Nicolas <danaelisa.nicolas@gmail.com>
2020-08-04 00:06:56 +1000

    Added disclaimer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 
 import { makeStyles } from '@material-ui/core/styles'
 
 import AppBar from '@material-ui/core/AppBar'
-import { Toolbar, Typography, Button, Box } from '@material-ui/core'
 import { ThemeContext } from '../contexts/ThemeContext'
+
+import { Toolbar, Typography, Button, Box, Dialog, Container } from '@material-ui/core'
 
 const Navbar = () => {
   const { isLightTheme, themes } = useContext(ThemeContext)
+  const [ open, setOpen ] = useState(false)
 
   const useStyles = makeStyles((theme) => ({
     navbar: {
@@ -37,17 +39,54 @@ const Navbar = () => {
       [theme.breakpoints.up('md')]: {
         fontSize: '1.05em',
       }
+    },
+    disclaimer: {
+      padding: '20px',
+      backgroundColor: isLightTheme ? themes.light.background : themes.dark.background,
+      color: isLightTheme ? themes.light.secondary : themes.dark.primary,
+    },
+    disclaimerTitle: {
+      fontWeight: 'bold',
+      paddingBottom: '8px',
+    },
+    nswButton: {
+      textTransform: 'none',
+      color: isLightTheme ? themes.light.primary : themes.dark.secondary,
+    },
+    buttonBox: {
+      width: '100%',
+      textAlign: 'center',
     }
   }))
 
   const styles = useStyles()
 
+  const aboutHandler = (e) => {
+    console.log('test ')
+    setOpen(true)
+  }
+
+  const closeHandler = () => {
+    setOpen(false)
+  }
+
+  const aboutBody = (
+    <Container className={styles.disclaimer}>
+      <Typography className={styles.disclaimerTitle} variant='h6'>Disclaimer</Typography>
+      <Typography variant='body2'>This project is by no means meant to replace announcements and data from the NSW Gov Health. If you would like to know more regarding the state's current COVID-19 situation, please visit:</Typography>
+      <Box className={styles.buttonBox}>
+        <Button className={styles.nswButton} href='https://www.nsw.gov.au/covid-19/latest-news-and-updates'>NSW Health News and Updates</Button>
+      </Box>
+    </Container>
+  )
+
   return (
     <AppBar position='static' className={styles.navbar}>
+      {<Dialog open={open} onClose={closeHandler}>{aboutBody}</Dialog>}
       <Toolbar variant='dense' className={styles.toolbar} boxShadow={0}>
         <Typography variant='h6' className={styles.brand}>NSW Covid Cases Checker</Typography>
         <Box className={styles.menu}>
-          <Button color='inherit'>About</Button>
+          <Button color='inherit' onClick={aboutHandler}>About</Button>
         </Box>
       </Toolbar>
     </AppBar>
