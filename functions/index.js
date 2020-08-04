1a2ae3dd954f8b431bffe6c1b9f5216b87f22df3
Dana Elisa Nicolas <danaelisa.nicolas@gmail.com>
2020-08-04 22:38:08 +1000

    functions set up

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,24 @@
 const functions = require('firebase-functions');
+const axios = require('axios');
+const cors = require('cors')({ origin: true });
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+exports.fetchData = functions.https.onRequest((request, response) => {
+  return cors(request, response, () => {
+    var postcode = request.query.postcode
+    axios.get(`https://data.nsw.gov.au/data/api/3/action/datastore_search?sort=notification_date desc&resource_id=2776dbb8-f807-4fb2-b1ed-184a6fc2c8aa&q=${postcode}`)
+      .then(res => {
+        response.status(200).json(res.data)
+      })
+      .catch(err => {
+        response.status(500).json({error: err})
+      })
+  })
+});
